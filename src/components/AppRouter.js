6b3e06ee8c23b672b9fe871508ee561c8c3ba9a2
Виктор <viktor@MacBook-Air-Viktor.local>
2021-09-20 17:43:59 +0300

    first commit

---
+++ src/components/AppRouter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+const AppRouter = () => {
+	return (
+		<div>
+			List
+		</div>
+	)
+}
+
+export default AppRouter;
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import List from "./pages/list";
+
+export const publicRoutes = {
+	path: '/List',
+	Component: List
+}
